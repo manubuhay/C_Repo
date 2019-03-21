377caf475df6fda6add99db2625b74b3fae6e68e
Manu Buhay <manubuhay@gmail.com>
2019-03-21 13:27:19 +0800

    edit array_del.c, added new c file string.c

---
+++ string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int i, c, MAX = 50;//declare
+char S[MAX];
+
+printf("Enter string:\n");
+scanf("%s",&S);//enter string
+
+printf("How many times do you want to enter the said string?\n");
+scanf("%d",&i); //counter how many times you want to print string
+
+for (c = 0; c < i; c++)
+printf("%s\n",S);//print string i number of times
+}
