43f7dc567dac839cb90c6af42ac50fcbe3edecab
Manu Buhay <manubuhay@gmail.com>
2019-03-21 14:01:56 +0800

    changed printf and scanf to puts and gets

--- string.c
+++ string.c
@@ -6,11 +6,14 @@ int i, c, MAX = 50;//declare
 char S[MAX];
 
 printf("Enter string:\n");
-scanf("%s",&S);//enter string
+//scanf("%s",&S);//enter string
+gets(S);
 
 printf("How many times do you want to enter the said string?\n");
 scanf("%d",&i); //counter how many times you want to print string
 
 for (c = 0; c < i; c++)
-printf("%s\n",S);//print string i number of times
+//printf("%s\n",S);//print string i number of times
+puts(S);
+printf("\n");
 }
