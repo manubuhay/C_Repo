0ffeaa0d672abc8d1eea162736e7e9263e8e7145
Manu Buhay <manubuhay@gmail.com>
2019-03-22 11:47:49 +0800

    edit code

--- string.c
+++ string.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+//#include <string.h>
 
 int main()
 {
-int i, c, MAX = 50;//declare
-char S[MAX];
+  int i, c, MAX = 50;//declare
+  char S[MAX];
 
-printf("Enter string:\n");
-//scanf("%s",&S);//enter string
-gets(S);
+  printf("Enter string:\n");
+  //scanf("%s",&S);//enter string
+  gets(S);
 
-printf("How many times do you want to enter the said string?\n");
-scanf("%d",&i); //counter how many times you want to print string
+  printf("How many times do you want to enter the said string?\n");
+  scanf("%d",&i); //counter how many times you want to print string
 
-for (c = 0; c < i; c++)
-//printf("%s\n",S);//print string i number of times
-puts(S);
-printf("\n");
+  for (c = 0; c < i; c++)
+    //printf("%s\n",S);//print string i number of times
+    puts(S);
+    printf("\n");
 }
