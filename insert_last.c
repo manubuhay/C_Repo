911c27fef0f11afee1c56d91a7bb9e5daf105343
Manu Buhay <manubuhay@gmail.com>
2019-03-27 11:15:52 +0800

    rename to insert last, kai insert first akong gibutang

--- insert_last.c
+++ insert_last.c
@@ -11,12 +11,16 @@ int main(void)
   if(total > 0 && total < 51 ) //total needs to be greater than 0 and less than 50
   {
     printf("Input %d numbers:\n",total);
+
     for(i = 0; i < total; i++) //input the numbers
       scanf("%d",&a[i]);
+
     printf("Input value to add:\n");
     scanf("%d", &add);//input which value you want to add
+
     a[total] = add;//adds value to end of the array
     total++;//increment total because a new value is added at the end
+
     printf("Values are:\n");
     for(i = 0; i < total; i++) //print the values in the array
       printf("%d\n",a[i]);
